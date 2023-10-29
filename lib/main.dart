e4e40c0e34a5b1d6a4c7643eb0cd917e65ada056
amorins <amorins@eisgroup.com>
2023-10-29 21:43:15 +0200

    Lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_test/location_list.dart';
 import 'package:my_flutter_test/mocks/mock_location.dart';
 import 'location_detail.dart';
 import 'mocks/mock_location.dart';
 import 'models/location.dart';
 
 void main() {
-  final Location mockLocation = MockLocation.FetchAny();
+  final mockLocations = MockLocation.fetchAll();
 
-  return runApp(MaterialApp(
-      home: LocationDetail(mockLocation)));
+  return runApp(MaterialApp(home: LocationList(mockLocations)));
 }
