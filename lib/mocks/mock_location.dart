3da498e430241b35845973d5a9d3334b56c33fb6
amorins <amorins@eisgroup.com>
2023-10-29 14:43:10 +0200

    images

--- lib/mocks/mock_location.dart
+++ lib/mocks/mock_location.dart
@@ -6,17 +6,17 @@ class MockLocation extends Location {
 
   static Location FetchAny() {
     return Location(
-        name: "testName1",
-        url: 'https://goo.gl/Npy2K8',
+        name: 'Arashiyama Bamboo Grove, Kyoto, Japan',
+        url: 'https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg',
         facts: <LocationFact>[
           LocationFact(
-              title: "Summary",
+              title: 'Summary',
               text:
-                  "adg dgdg dag adg dagd gd dag dag dag d ad gdag dag d d da dag"),
+              'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm'),
           LocationFact(
-              title: "Summary2",
+              title: 'How to Get There',
               text:
-                  "adg dgdg dag adg dagd gd dag dag dag d ad gdag dag d d da dag"),
+              'Kyoto airport, with several terminals, is located 16 kilometres south of the city and is also known as Kyoto. Kyoto can also be reached by transport links from other regional airports.')
         ]);
   }
 }
